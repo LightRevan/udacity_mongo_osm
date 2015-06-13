c3d2ab794848d23b8ee7bffaa625f0f429f0a834
lightrevan <lightrevan@yandex.ru>
2015-06-13 18:34:00 +0300

    implemented loading tag data into mongo

--- dbconnection.py
+++ dbconnection.py
@@ -2,5 +2,28 @@
 
 import pymongo
 
-client = pymongo.MongoClient
+client = pymongo.MongoClient()
 db = client.osm
+# db.drop_collection('meta')
+# db.meta.create_index('file')
+# db.meta.create_index('element')
+# db.meta.create_index('tag.name')
+# db.meta.create_index('tag.count')
+
+# test = client.test.test
+# client.drop_database('test')
+
+# test.delete_many({})
+
+# test.insert_one({'ar': [{'f':1}, {'f': 2}]})
+# test.insert_one({'ar': [{'f':3}]})
+# test.insert_one({'ar': [{'f':3}, {'f':2}]})
+#
+# for o in test.find({'ar.f': 2}):
+#     print o
+
+# test.update_one({'f.name': 1, 'f.f.name': 1}, {'$addToSet': {'f.f.f': {'name': 1}}}, upsert=True)
+# test.update_one({'f.name': 1, 'f.f.name': 2}, {'$addToSet': {'f.f.f': {'name': 1}}}, upsert=True)
+#
+# for o in test.find():
+#     print o
--- file_parser.py
+++ file_parser.py
@@ -3,15 +3,55 @@
 import xml.sax as sax
 import collections
 
+from dbconnection import *
+
 
 class OSMHandler(sax.ContentHandler):
-    def __init__(self):
+    def __init__(self, fname, db):
         sax.ContentHandler.__init__(self)
+
+        self.fname = fname
+        self.db = db
+
         self.attribute_dict = collections.defaultdict(set)
         self.subelements = collections.defaultdict(set)
         self.open_elements = []
 
         self.interesting_elements = ['node', 'way', 'tag', 'nd']
+
+    def save_to_db(self, elem, k, v):
+        parts = k.split(':')
+        query = {'file': self.fname, 'element': elem, 'tag.name': parts[0]}
+        doc = self.db.find_one(query)
+        is_changed = False
+        if not doc:
+            is_changed = True
+            doc = {'file': self.fname, 'element': elem, 'tag': {'name': parts[0], 'count': 0, 'examples': [v]}}
+
+        cur_tag = doc['tag']
+        cur_tag['count'] += 1
+        for tag_part in parts[1:]:
+            subtags = cur_tag.get('subtags', None)
+            if subtags is None:
+                subtags = []
+                cur_tag['subtags'] = subtags
+
+            for tag in subtags:
+                if tag['name'] == tag_part:
+                    cur_tag = tag
+                    cur_tag['count'] += 1
+                    break
+            else:
+                cur_tag = {'name': tag_part, 'count': 1, 'examples': []}
+                subtags.append(cur_tag)
+                is_changed = True
+
+        if is_changed:
+            examples = cur_tag['examples']
+            if len(examples) < 11 and v not in examples:
+                examples.append(v)
+
+            res = self.db.replace_one(query, doc, upsert=True)
 
     def startElement(self, name, attrs):
         if name not in self.interesting_elements:
@@ -24,6 +64,9 @@ class OSMHandler(sax.ContentHandler):
         if len(self.open_elements) > 1:
             self.subelements[self.open_elements[-2]].add(name)
 
+        if len(self.open_elements) > 1 and name == 'tag':
+            self.save_to_db(self.open_elements[-2], attrs.getValue('k'), attrs.getValue('v'))
+
     def endElement(self, name):
         if name in self.interesting_elements:
             self.open_elements.pop()
@@ -31,10 +74,13 @@ class OSMHandler(sax.ContentHandler):
 if __name__ == '__main__':
     fname = 'data/moscow_russia.osm'
 
+    db.meta.delete_many({'file': fname})
+
     tags = set()
     with open(fname, 'r') as f:
-        handler = OSMHandler()
+        handler = OSMHandler(fname, db.meta)
         sax.parse(f, handler)
 
     print handler.attribute_dict
     print handler.subelements
+    print db.meta.count()
